43361e4f45f00b8e16cb00bdba3c2d7db39aa5ad
fatima-28 <2n6gddb@code.edu.az>
2023-05-27 12:06:26 -0700

    Crud methods added to controller

--- HospitalManagementSystem/HospitalManagementSystem/Areas/AdminPanel/Controllers/HospitalController.cs
+++ HospitalManagementSystem/HospitalManagementSystem/Areas/AdminPanel/Controllers/HospitalController.cs
@@ -1,4 +1,5 @@
 ﻿using HospitalManagementSystem.Services;
+using HospitalManagementSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalManagementSystem.Areas.AdminPanel.Controllers
@@ -11,9 +12,48 @@ namespace HospitalManagementSystem.Areas.AdminPanel.Controllers
         {
             _hospitalService = hospitalService;
         }
-        public IActionResult Index()
+        public IActionResult Index(int pageNum=1,int pageSize=10)
         {
-            return View();
+            return View(_hospitalService.GetAll(pageNum, pageSize));
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Create(HospitalVM vm)
+        {
+            _hospitalService.Add(vm);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var vm = _hospitalService.GetById(id);
+            return View(vm);
+            
+        }
+        [HttpPost]
+        public IActionResult Edit(HospitalVM vm)
+        {
+           _hospitalService.Edit(vm);
+            return RedirectToAction("Index");
+
+        }
+
+      
+        public IActionResult Delete(int id)
+        {
+            _hospitalService.Delete(id);
+            return RedirectToAction("Index");
+
+        }
+
+
     }
 }
